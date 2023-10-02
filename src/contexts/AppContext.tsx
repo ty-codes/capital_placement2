4101c87c7a4d79fdbb5aefb94b88445a31113688
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-10-01 21:25:12 -0800

    refactor - update states in app context

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useContext, useEffect, useState } from 'react';
 import { AppContextType, IForm, IQuestion } from '../@types/app';
+import uuid from 'react-uuid';
 
 export const AppContext = createContext<AppContextType | null>(null);
 
@@ -8,10 +9,26 @@ export default function AppProvider({ children }: { children: ReactNode }) {
     const [profile, setProfile] = useState<IForm | null>(null);
     const [personalInformation, setPersonalInformation] = useState<IForm | null>(null);
     const [customizedQuestions, setCustomisedQuestions] = useState<IQuestion[] | []>([]);
+    const [imageUri, setImageUri] = useState<string>('');
+
+
     const [profileTypes, setProfileTypes] = useState<string[] | []>([])
     const [personalTypes, setPersonalTypes] = useState<string[] | []>([])
     const [customisedTypes, setCustomisedTypes] = useState<string[] | []>([])
+    
 
+    const [submitted, setSubmitted] = useState<boolean>(false);
+
+    const attributes = {
+        id: uuid(),
+        attributes: {
+            coverImage: imageUri,
+            personalInformation,
+            profile,
+            customizedQuestions
+        }
+    }
+    
     return (
         <AppContext.Provider value={{
             profile,
@@ -25,7 +42,12 @@ export default function AppProvider({ children }: { children: ReactNode }) {
             personalTypes,
             profileTypes,
             customisedTypes,
-            setCustomisedTypes
+            setCustomisedTypes,
+            setImageUri,
+            imageUri,
+            attributes,
+            submitted,
+            setSubmitted
         }}>
             {children}
         </AppContext.Provider>
